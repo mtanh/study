fd0102c34230f2a4ae5147cf8bc1bd1b1fa850eb
Anh Ma <anhma282@gmail.com>
2016-05-01 16:23:59 +0700

    01/05/2016

--- common/defines.hpp
+++ common/defines.hpp
@@ -11,7 +11,4 @@
 #define WHICHFUNC fprintf (stderr, "%s\n", __FUNCTION__);
 #define WHICHLINE fprintf (stderr, "%d\n", __LINE__);
 
-typedef enum { True, False } Boolean;
-
-
 #endif /* COMMON_DEFINES_HPP_ */
