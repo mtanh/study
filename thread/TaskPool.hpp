ea648289c366aeda1fbab5d612ebd059f3c695c5
Anh Ma <anhma282@gmail>
2016-05-24 17:37:48 +0700

    Priority

--- thread/TaskPool.hpp
+++ thread/TaskPool.hpp
@@ -26,278 +26,342 @@
 
 static const unsigned int MAX_TASK = 10;
 
-class LocalTaskPool: public boost::noncopyable {
+class LocalTaskPool : public boost::noncopyable
+{
 
-	typedef std::deque<CallableBase*> TASK_LIST;
-	typedef std::vector<WorkerThread*> THREAD_LIST;
+  typedef std::deque<CallableBase*> TASK_LIST;
+  typedef std::vector<WorkerThread*> THREAD_LIST;
 
-	template<typename T>
-	class ThreadStateCompare {
-	public:
-		bool operator()(const T* const t1, const T* const t2) {
-			return StateLess(t1->GetThreadState(), t2->GetThreadState());
-		}
-		bool operator()(const T* const t, ThreadState state) {
-			return StateLess(t->GetThreadState(), state);
-		}
-		bool operator()(ThreadState state, const T* const t) {
-			return StateLess(t->GetThreadState(), state);
-		}
-	private:
-		bool StateLess(ThreadState state1, ThreadState state2) const {
-			return (state1 < state2);
-		}
-	};
+  template<typename T>
+    class ThreadStateCompare
+    {
+    public:
+      bool
+      operator()(const T* const t1, const T* const t2)
+      {
+        return StateLess(t1->GetThreadState(), t2->GetThreadState());
+      }
+      bool
+      operator()(const T* const t, ThreadState state)
+      {
+        return StateLess(t->GetThreadState(), state);
+      }
+      bool
+      operator()(ThreadState state, const T* const t)
+      {
+        return StateLess(t->GetThreadState(), state);
+      }
+    private:
+      bool
+      StateLess(ThreadState state1, ThreadState state2) const
+      {
+        return (state1 < state2);
+      }
+    };
 
-	typedef struct SynchSuite {
-		boost::mutex mtxThreadsMap;
-		boost::mutex mtxTaskQueue;
-		boost::condition_variable cvTaskQueue;
-	} SynchSuite;
+  typedef struct SynchSuite
+  {
+    boost::mutex mtxThreadsMap;
+    boost::mutex mtxTaskQueue;
+    boost::condition_variable cvTaskQueue;
+  } SynchSuite;
 
-	class GetTask: public CallableBase {
-	public:
-		GetTask(): CallableBase() {}
-		GetTask(void* arg, CallablePriority priority = CALLABLE_PRIORITY_NORMAL): CallableBase(arg, priority) {}
-		virtual ~GetTask() {}
-		virtual void operator()() {
-			LocalTaskPool* pTaskPool = (LocalTaskPool*)m_arg;
-			if(pTaskPool != nullptr) {
+  class GetTask : public CallableBase
+  {
+  public:
+    GetTask() :
+      CallableBase()
+    {
+    }
+    GetTask(void* arg, CallablePriority priority = CALLABLE_PRIORITY_NORMAL) :
+      CallableBase(arg, priority)
+    {
+    }
+    virtual
+    ~GetTask()
+    {
+    }
+    virtual void
+    operator()()
+    {
+      LocalTaskPool* pTaskPool = (LocalTaskPool*) m_arg;
+      if (pTaskPool != nullptr)
+        {
+          while (!pTaskPool->m_isStopped)
+            {
+              CallableBase* task = pTaskPool->PopTask();
+              if (task)
+                {
+                  std::cout << "Thread " << boost::this_thread::get_id()
+                      << " pop " << task << "\n";
+                  (*task)();
+                  boost::this_thread::sleep_for(
+                      boost::chrono::milliseconds(5000));
+                  std::cout << "Thread " << boost::this_thread::get_id()
+                      << " processed " << task << "\n";
+                }
+            }
+        }
+    }
+  };
 
-				while(!pTaskPool->m_isStopped) {
+  typedef struct MostInsigfinicantTaskPred
+  {
+    bool
+    operator()(CallableBase* task)
+    {
+      assert(task != nullptr);
+      return (task->Priority() == CALLABLE_PRIORITY_LOWEST);
+    }
+  } MostInsigfinicantTaskPred;
 
-					CallableBase* task = pTaskPool->PopTask();
-					if(task) {
-						std::cout << "Thread " << boost::this_thread::get_id() << " pop " << task << "\n";
-						(*task)();
-						boost::this_thread::sleep_for(boost::chrono::milliseconds(5000));
-						std::cout << "Thread " << boost::this_thread::get_id() << " processed " << task << "\n";
-					}
-				}
-			}
-		}
-	};
-
-	typedef struct MostInsigfinicantTaskPred {
-
-		bool operator()(CallableBase* task) {
-			assert(task != nullptr);
-			return (task->Priority() == CALLABLE_PRIORITY_LOWEST);
-		}
-	} MostInsigfinicantTaskPred;
-
-	/*
-	class ThreadMonitorTask: public CallableBase {
-	public:
-		ThreadMonitorTask(): CallableBase() {}
-		ThreadMonitorTask(void* arg, CallablePriority priority = CALLABLE_PRIORITY_NORMAL): CallableBase(arg, priority) {}
-		virtual ~ThreadMonitorTask() {}
-		virtual void operator()() {
-			TaskPool* pTaskPool = (TaskPool*)m_arg;
-			if(pTaskPool != nullptr) {
-				while(!pTaskPool->m_isStopped) {
-				}
-			}
-		}
-	};
-	*/
+  /*
+   class ThreadMonitorTask: public CallableBase {
+   public:
+   ThreadMonitorTask(): CallableBase() {}
+   ThreadMonitorTask(void* arg, CallablePriority priority = CALLABLE_PRIORITY_NORMAL): CallableBase(arg, priority) {}
+   virtual ~ThreadMonitorTask() {}
+   virtual void operator()() {
+   TaskPool* pTaskPool = (TaskPool*)m_arg;
+   if(pTaskPool != nullptr) {
+   while(!pTaskPool->m_isStopped) {
+   }
+   }
+   }
+   };
+   */
 
 public:
-	LocalTaskPool();
-	~LocalTaskPool();
+  LocalTaskPool();
+  ~LocalTaskPool();
 
-	bool Init(size_t maxThreadNum = (size_t)boost::thread::hardware_concurrency());
-	void Start();
-	void Stop();
-	inline void Run(CallableBase* task);
-	inline bool Stopped() const { return m_isStopped; }
-	bool RemoveMostInsigfinicantTasks();
-	void PushTask(CallableBase* task);
-	CallableBase* PopTask();
+  bool
+  Init(size_t maxThreadNum = (size_t) boost::thread::hardware_concurrency());
+  void
+  Start();
+  void
+  Stop();
+  inline void
+  Run(CallableBase* task);
+  inline bool
+  Stopped() const
+  {
+    return m_isStopped;
+  }
+  bool
+  RemoveMostInsigfinicantTasks();
+  void
+  PushTask(CallableBase* task);
+  CallableBase*
+  PopTask();
 
 private:
-	void CreateAndStartThread();
+  void
+  CreateAndStartThread();
 
 private:
-	size_t m_maxThreads;
-	boost::atomic<size_t> m_numRunningThreads;
-	boost::atomic_bool m_isStopped;
+  size_t m_maxThreads;
+  boost::atomic<size_t> m_numRunningThreads;
+  boost::atomic_bool m_isStopped;
 
-	TASK_LIST m_taskQueue;
-	THREAD_LIST m_runningThreadsMap;
-	WorkerThread m_threadMonitoring;
-	SynchSuite m_synchSuite;
+  TASK_LIST m_taskQueue;
+  THREAD_LIST m_runningThreadsMap;
+  WorkerThread m_threadMonitoring;
+  SynchSuite m_synchSuite;
 };
 
-
-LocalTaskPool::LocalTaskPool()
-: m_maxThreads((unsigned int) boost::thread::hardware_concurrency())
-, m_isStopped(true)
-, m_numRunningThreads(0) {
+LocalTaskPool::LocalTaskPool() :
+  m_maxThreads((unsigned int) boost::thread::hardware_concurrency()),
+      m_isStopped(true), m_numRunningThreads(0)
+{
 }
 
-bool LocalTaskPool::Init(size_t maxThreadNum) {
+bool
+LocalTaskPool::Init(size_t maxThreadNum)
+{
+  bool ret = true;
+  do
+    {
+      if (maxThreadNum <= 0 || maxThreadNum >= UINT_MAX)
+        {
+          ret = false;
+          break;
+        }
+      m_maxThreads = maxThreadNum;
+    }
+  while (false);
 
-	bool ret = true;
-	do {
-		if (maxThreadNum <= 0 || maxThreadNum >= UINT_MAX) {
-			ret = false;
-			break;
-		}
-		m_maxThreads = maxThreadNum;
-
-	} while (false);
-
-	return ret;
+  return ret;
 }
 
-LocalTaskPool::~LocalTaskPool() {
+LocalTaskPool::~LocalTaskPool()
+{
+  WHICHFUNC
+  THREAD_LIST::iterator it = m_runningThreadsMap.begin();
+  for (; it != m_runningThreadsMap.end(); ++it)
+    {
+      delete (*it);
+      (*it) = nullptr;
+    }
 
-	WHICHFUNC
-	THREAD_LIST::iterator it = m_runningThreadsMap.begin();
-	for(; it != m_runningThreadsMap.end(); ++it) {
-		delete (*it);
-		(*it) = nullptr;
-	}
-
-	TASK_LIST::iterator it1 = m_taskQueue.begin();
-	for(; it1 != m_taskQueue.end(); ++it1) {
-		delete (*it1);
-		(*it1) = nullptr;
-	}
+  TASK_LIST::iterator it1 = m_taskQueue.begin();
+  for (; it1 != m_taskQueue.end(); ++it1)
+    {
+      delete (*it1);
+      (*it1) = nullptr;
+    }
 }
 
-inline void LocalTaskPool::Start() {
+inline void
+LocalTaskPool::Start()
+{
+  if (m_isStopped)
+    {
+      m_isStopped = false; // start or restart task pool
+      CreateAndStartThread();
 
-	if (m_isStopped) {
-		m_isStopped = false; // start or restart task pool
-		CreateAndStartThread();
-
-		/*
-		ThreadMonitorTask* monitorTask = new ThreadMonitorTask(this);
-		m_threadMonitoring.SetCallable(monitorTask);
-		m_threadMonitoring.Start();
-		std::cout << "[Started] m_threadMonitoring ID= " << m_threadMonitoring.GetThreadId() << "\n";
-		*/
-	}
+      /*
+       ThreadMonitorTask* monitorTask = new ThreadMonitorTask(this);
+       m_threadMonitoring.SetCallable(monitorTask);
+       m_threadMonitoring.Start();
+       std::cout << "[Started] m_threadMonitoring ID= " << m_threadMonitoring.GetThreadId() << "\n";
+       */
+    }
 }
 
-void LocalTaskPool::Stop() {
+void
+LocalTaskPool::Stop()
+{
+  if (!m_isStopped)
+    {
+      m_isStopped = true;
 
-	if(!m_isStopped) {
-		m_isStopped = true;
+      m_synchSuite.cvTaskQueue.notify_all();
+      std::cout << "Thread: " << m_threadMonitoring.GetThreadId()
+          << " stopped\n";
+      m_threadMonitoring.Stop();
 
-		m_synchSuite.cvTaskQueue.notify_all();
-		std::cout << "Thread: " << m_threadMonitoring.GetThreadId() << " stopped\n";
-		m_threadMonitoring.Stop();
-
-		THREAD_LIST::iterator it = m_runningThreadsMap.begin();
-		for(; it != m_runningThreadsMap.end(); ++it) {
-			std::cout << "Thread: " << (*it)->GetThreadId() << " stopped\n";
-			(*it)->Stop();
-		}
-	}
+      THREAD_LIST::iterator it = m_runningThreadsMap.begin();
+      for (; it != m_runningThreadsMap.end(); ++it)
+        {
+          std::cout << "Thread: " << (*it)->GetThreadId() << " stopped\n";
+          (*it)->Stop();
+        }
+    }
 }
 
-inline void LocalTaskPool::Run(CallableBase* task) {
-
-	if(!m_isStopped) {
-		PushTask(task);
-	}
+inline void
+LocalTaskPool::Run(CallableBase* task)
+{
+  if (!m_isStopped)
+    {
+      PushTask(task);
+    }
 }
 
-void LocalTaskPool::PushTask(CallableBase* task) {
+void
+LocalTaskPool::PushTask(CallableBase* task)
+{
+  do
+    {
+      if (nullptr == task)
+        {
+          break;
+        }
 
-	do {
+      // check and start a thread in order to wait the incoming task
+      if (m_numRunningThreads.load() < m_maxThreads)
+        {
+          CreateAndStartThread();
+        }
 
-		if(nullptr == task) {
-			break;
-		}
+      boost::unique_lock<boost::mutex> lock(m_synchSuite.mtxTaskQueue);
+      if ((size_t) m_taskQueue.size() == MAX_TASK)
+        {
+          if (task->Priority() == CALLABLE_PRIORITY_LOWEST)
+            {
+              std::cout << "IGNORED " << task << "[" << task->Priority() << "]"
+                  << "\n";
+              break;
+            }
+          else
+            {
+              if (!RemoveMostInsigfinicantTasks())
+                {
+                  break;
+                }
+            }
+        }
 
-		// check and start a thread in order to wait the incoming task
-		if(m_numRunningThreads.load() < m_maxThreads) {
-			CreateAndStartThread();
-		}
-
-		boost::unique_lock<boost::mutex> lock(m_synchSuite.mtxTaskQueue);
-		if((size_t)m_taskQueue.size() == MAX_TASK) {
-			if(task->Priority() == CALLABLE_PRIORITY_LOWEST) {
-				std::cout << "IGNORED " << task << "[" << task->Priority() << "]" << "\n";
-				break;
-			}
-			else {
-				if(!RemoveMostInsigfinicantTasks()) {
-					break;
-				}
-			}
-		}
-
-		m_taskQueue.push_back(task);
-		std::cout << "PUSHED " << task << "[" << task->Priority() << "]" << "\n";
-		m_synchSuite.cvTaskQueue.notify_one();
-
-	} while(false);
+      m_taskQueue.push_back(task);
+      std::cout << "PUSHED " << task << "[" << task->Priority() << "]" << "\n";
+      m_synchSuite.cvTaskQueue.notify_one();
+    }
+  while (false);
 }
 
-CallableBase* LocalTaskPool::PopTask() {
+CallableBase*
+LocalTaskPool::PopTask()
+{
+  CallableBase* pTask = nullptr;
+    {
+      boost::unique_lock<boost::mutex> lock(m_synchSuite.mtxTaskQueue);
+      while (m_taskQueue.empty())
+        {
+          puts("Wait for task ...");
+          m_synchSuite.cvTaskQueue.wait(lock);
+        }
 
-	CallableBase* pTask = nullptr;
-	{
-		boost::unique_lock<boost::mutex> lock(m_synchSuite.mtxTaskQueue);
-		while(m_taskQueue.empty()) {
-			puts("Wait for task ...");
-			m_synchSuite.cvTaskQueue.wait(lock);
-		}
+      // Double check to avoid the fake notifications
+      if (m_taskQueue.empty())
+        {
+          lock.unlock();
+          return nullptr;
+        }
 
-		// Double check to avoid the fake notifications
-		if(m_taskQueue.empty()) {
-			lock.unlock();
-			return nullptr;
-		}
-
-		pTask = m_taskQueue.front();
-		m_taskQueue.pop_front();
-	}
-	return pTask;
+      pTask = m_taskQueue.front();
+      m_taskQueue.pop_front();
+    }
+  return pTask;
 }
 
-bool LocalTaskPool::RemoveMostInsigfinicantTasks() {
-
-	TASK_LIST::iterator iter = std::find_if(m_taskQueue.begin(), m_taskQueue.end(), MostInsigfinicantTaskPred());
-	if(iter != m_taskQueue.end()) {
-		(void)m_taskQueue.erase(iter);
-		std::cout << "DELETED " << (*iter) << "[" << (*iter)->Priority() << "]" << "\n";
-		return true;
-	}
-	return false;
+bool
+LocalTaskPool::RemoveMostInsigfinicantTasks()
+{
+  TASK_LIST::iterator iter = std::find_if(m_taskQueue.begin(),
+      m_taskQueue.end(), MostInsigfinicantTaskPred());
+  if (iter != m_taskQueue.end())
+    {
+      (void) m_taskQueue.erase(iter);
+      std::cout << "DELETED " << (*iter) << "[" << (*iter)->Priority() << "]"
+          << "\n";
+      return true;
+    }
+  return false;
 }
 
-void LocalTaskPool::CreateAndStartThread() {
+void
+LocalTaskPool::CreateAndStartThread()
+{
+  // protect the whole transaction utilize lock_guard
+  if (!m_isStopped)
+    {
+      WorkerThread* thr = new WorkerThread(THREAD_PRIORITY_ABOVE_NORMAL, /*auto-run=*/
+      false);
+      GetTask* getTask = new GetTask(this);
+      thr->SetCallable(getTask);
+      (void) thr->Start();
 
-	// protect the whole transaction utilize lock_guard
-	if(!m_isStopped) {
-		WorkerThread* thr = new WorkerThread(THREAD_PRIORITY_ABOVE_NORMAL, /*auto-run=*/false);
-		GetTask* getTask = new GetTask(this);
-		thr->SetCallable(getTask);
-		(void)thr->Start();
-
-		// TODO Leak ???
-		//boost::shared_ptr<WorkerThread> thr(new WorkerThread(THREAD_PRIORITY_ABOVE_NORMAL, /*auto-run=*/false));
-		//boost::shared_ptr<WorkerTask> w(new WorkerTask(this));
-		//thr->SetCallableObj(w.get());
-
-		{
-			boost::lock_guard<boost::mutex> guard(m_synchSuite.mtxThreadsMap);
-			m_runningThreadsMap.push_back(thr);
-			/*
-			std::sort(m_runningThreadsMap.begin(), m_runningThreadsMap.end(),
-					ThreadStateCompare<WorkerThread>());
-			*/
-		}
-		//m_numRunningThreads++;
-		m_numRunningThreads.fetch_add(1);
-		//printf("m_numRunningThreads: %u\n", m_numRunningThreads.load());
-	}
+        {
+          boost::lock_guard<boost::mutex> guard(m_synchSuite.mtxThreadsMap);
+          m_runningThreadsMap.push_back(thr);
+          /*
+           std::sort(m_runningThreadsMap.begin(), m_runningThreadsMap.end(),
+           ThreadStateCompare<WorkerThread>());
+           */
+        }
+      m_numRunningThreads.fetch_add(1);
+    }
 }
 
 extern LocalTaskPool gTaskPool;
--- thread/test_main.cxx
+++ thread/test_main.cxx
@@ -23,8 +23,11 @@
 #include <string>
 #include <signal.h>
 #include <semaphore.h>
+#include <map>
+#include <unordered_map>
 #include <boost/ptr_container/ptr_vector.hpp>
 #include <boost/container/vector.hpp>
+#include <boost/container/deque.hpp>
 #include <boost/container/list.hpp>
 #include <boost/intrusive/list.hpp>
 #include <boost/thread/locks.hpp>
@@ -32,162 +35,186 @@
 #include <boost/shared_ptr.hpp>
 #include <boost/make_shared.hpp>
 #include <boost/interprocess/shared_memory_object.hpp>
+#include <boost/heap/priority_queue.hpp>
 
 #include "TaskPool.hpp"
 
 LocalTaskPool gTaskPool;
 
-class ATask: public CallableBase {
+class ThreadSem : boost::noncopyable
+{
 public:
-	ATask(): CallableBase() {}
-	ATask(void* arg, CallablePriority priority = CALLABLE_PRIORITY_NORMAL): CallableBase(arg, priority) {}
-	virtual ~ATask() {}
-	void operator()() {
+  ThreadSem()
+  {
+  }
+  ~ThreadSem()
+  {
+  }
 
-	}
-};
+  void
+  Init(int c)
+  {
+    counter = c;
+  }
 
-class ThreadSem: boost::noncopyable {
+  void
+  Wait()
+  {
+    boost::unique_lock<boost::mutex> lock(mtx);
+    while (counter < 0)
+      {
+        cond.wait(lock);
+      }
+    counter--;
+  }
 
-public:
-	ThreadSem() {}
-	~ThreadSem() {}
-
-	void Init(int c) { counter = c; }
-
-	void Wait() {
-		boost::unique_lock<boost::mutex> lock(mtx);
-		while(counter < 0) {
-			cond.wait(lock);
-		}
-		counter--;
-	}
-
-	void Post() {
-		boost::unique_lock<boost::mutex> lock(mtx);
-		counter++;
-		cond.notify_all();
-	}
+  void
+  Post()
+  {
+    boost::unique_lock<boost::mutex> lock(mtx);
+    counter++;
+    cond.notify_all();
+  }
 
 private:
-	int counter;
-	boost::condition_variable cond;
-	boost::mutex mtx;
+  int counter;
+  boost::condition_variable cond;
+  boost::mutex mtx;
 };
 
-typedef struct rw_lock_t {
-	int reader;
-	ThreadSem sem;
-	boost::mutex mtx;
+typedef struct rw_lock_t
+{
+  int reader;
+  ThreadSem sem;
+  boost::mutex mtx;
 } rw_lock_t;
 
-void InitRWLock(rw_lock_t* rw_lock) {
-	if(rw_lock != NULL) {
-		rw_lock->reader = 0;
-		rw_lock->sem.Init(/*num_of_writer=*/1);
-	}
-}
-
-void ReadLockAcquire(rw_lock_t* rw_lock) {
-	assert(rw_lock != NULL);
-	boost::lock_guard<boost::mutex> lock(rw_lock->mtx);
-	rw_lock->reader++;
-	if(1 == rw_lock->reader) {
-		rw_lock->sem.Wait();
-	}
-}
-
-void ReadLockRelease(rw_lock_t* rw_lock) {
-	assert(rw_lock != NULL);
-	boost::lock_guard<boost::mutex> lock(rw_lock->mtx);
-	rw_lock->reader--;
-	if(0 == rw_lock->reader) {
-		rw_lock->sem.Post();
-	}
-}
-
-void WriteLockAcquire(rw_lock_t* rw_lock) {
-	assert(rw_lock != NULL);
-	rw_lock->sem.Wait();
-}
-
-void WriteLockRelease(rw_lock_t* rw_lock) {
-	assert(rw_lock != NULL);
-	rw_lock->sem.Post();
-}
-
-void f1() {
-	static boost::thread_specific_ptr<int> tls1;
-	if(!tls1.get()) {
-		tls1.reset(new int(1));
-		std::cout << &tls1 << "\n";
-		std::cout << *tls1 << "\n";
-	}
-}
-
-void f2() {
-	static boost::thread_specific_ptr<int> tls;
-	if(!tls.get()) {
-		tls.reset(new int(2));
-		std::cout << &tls << "\n";
-		std::cout << *tls << "\n";
-	}
-}
-
-void cleaner(int* val) {
-	return;
-}
-
-void f111() {
-	boost::thread_specific_ptr<int> tmp(cleaner);
-	if(!tmp.get()) {
-		int a = 10;
-		tmp.reset(&a);
-	}
-}
-
-struct CallableSt {
-
-	typedef struct thread_spec_data {
-		boost::thread::id id;
-	} thread_spec_data;
-	boost::thread_specific_ptr<thread_spec_data> thread_spec_ptr_;
-
-	void operator()() {
-		if(thread_spec_ptr_.get() != NULL) {
-			thread_spec_data data;
-			data.id = boost::this_thread::get_id();
-			thread_spec_ptr_.reset(&data);
-			std::cout << "Thread ID: " << thread_spec_ptr_->id << "\n";
-		}
-	}
-
-};
-
-void thread()
+void
+InitRWLock(rw_lock_t* rw_lock)
 {
-	typedef struct thread_spec_data {
-		boost::thread::id id;
-	} thread_spec_data;
-	boost::thread_specific_ptr<thread_spec_data> thread_spec_ptr_;
-
-	try
-	{
-		if(thread_spec_ptr_.get() == NULL) {
-			thread_spec_data data;
-			data.id = boost::this_thread::get_id();
-			thread_spec_ptr_.reset(&data);
-			std::cout << "Thread ID: " << thread_spec_ptr_->id << "\n";
-		}
-	}
-	catch (boost::thread_interrupted&) {}
+  if (rw_lock != NULL)
+    {
+      rw_lock->reader = 0;
+      rw_lock->sem.Init(/*num_of_writer=*/1);
+    }
 }
 
-int main(int argc, char *argv[]) {
+void
+ReadLockAcquire(rw_lock_t* rw_lock)
+{
+  assert(rw_lock != NULL);
+  boost::lock_guard<boost::mutex> lock(rw_lock->mtx);
+  rw_lock->reader++;
+  if (1 == rw_lock->reader)
+    {
+      rw_lock->sem.Wait();
+    }
+}
 
-	boost::thread t{thread};
-	//t.interrupt();
-	t.join();
+void
+ReadLockRelease(rw_lock_t* rw_lock)
+{
+  assert(rw_lock != NULL);
+  boost::lock_guard<boost::mutex> lock(rw_lock->mtx);
+  rw_lock->reader--;
+  if (0 == rw_lock->reader)
+    {
+      rw_lock->sem.Post();
+    }
+}
 
-	return (EXIT_SUCCESS);
+void
+WriteLockAcquire(rw_lock_t* rw_lock)
+{
+  assert(rw_lock != NULL);
+  rw_lock->sem.Wait();
+}
+
+void
+WriteLockRelease(rw_lock_t* rw_lock)
+{
+  assert(rw_lock != NULL);
+  rw_lock->sem.Post();
+}
+
+template<typename T>
+  class CallablePrioprityCompare
+  {
+  public:
+    bool
+    operator()(/*const*/ T* /*const*/ t1, /*const*/ T* /*const*/ t2)
+    {
+      return PriorityGreat(t1->Priority(), t2->Priority());
+    }
+    bool
+    operator()(const T* const t, CallablePriority priority)
+    {
+      return PriorityGreat(t->Priority(), priority);
+    }
+    bool
+    operator()(CallablePriority priority, const T* const t)
+    {
+      return PriorityGreat(t->Priority(), priority);
+    }
+  private:
+    bool
+    PriorityGreat(CallablePriority priority1, CallablePriority priority2) const
+    {
+      return (priority1 < priority2);
+    }
+  };
+
+class ATask : public CallableBase
+  {
+  public:
+    ATask() :
+      CallableBase()
+    {
+    }
+    ATask(char* taskName, void* arg, CallablePriority priority = CALLABLE_PRIORITY_NORMAL) :
+      CallableBase(arg, priority)
+    , m_taskName(taskName)
+    {
+    }
+    virtual
+    ~ATask()
+    {
+    }
+    const char*
+    GetName() const
+    {
+      return m_taskName;
+    }
+    virtual void
+    operator()()
+    {
+      GetName();
+    }
+
+  private:
+    char* m_taskName;
+  };
+
+int
+main(int argc, char *argv[])
+{
+  /*
+  gTaskPool.Init(2);
+  gTaskPool.Start();
+  */
+
+  boost::heap::priority_queue<CallableBase*, boost::heap::compare<CallablePrioprityCompare<CallableBase> > > pq;
+  pq.push(new ATask("A", nullptr, CALLABLE_PRIORITY_HIGHEST));
+  pq.push(new ATask("B", nullptr));
+  pq.push(new ATask("C", nullptr, CALLABLE_PRIORITY_ABOVE_NORMAL));
+
+  /*
+  while(1)
+    {
+
+    }
+  gTaskPool.Stop();
+  */
+
+  return (EXIT_SUCCESS);
 }
